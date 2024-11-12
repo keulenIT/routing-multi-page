8edcba3de9e583c868c5098dd53795482e12863e
Mathias Keulen <mathias.keulen@external.engie.com>
2024-11-12 17:54:01 +0100

    feat(routing): use routerLink

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,13 @@
 import { Routes } from '@angular/router';
 import { TasksComponent } from './tasks/tasks.component';
+import { AppComponent } from './app.component';
+import { NoTaskComponent } from './tasks/no-task/no-task.component';
 
 export const routes: Routes = [
+  {
+    path: '',
+    component: NoTaskComponent,
+  },
   {
     path: 'tasks',
     component: TasksComponent,
