f33e716079166944c08f172647d7f04be6fac3e6
Mathias Keulen <mathias.keulen@external.engie.com>
2024-11-17 18:26:14 +0100

    feat(routing): load tasks

--- src/app/users/user-tasks/user-tasks.component.ts
+++ src/app/users/user-tasks/user-tasks.component.ts
@@ -1,29 +1,19 @@
 import { Component, computed, inject, input } from '@angular/core';
-import { User } from '../user/user.model';
 import { UsersService } from '../users.service';
-import { TasksService } from '../../tasks/tasks.service';
-import { RouterOutlet } from '@angular/router';
+import { RouterOutlet, RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-user-tasks',
   standalone: true,
   templateUrl: './user-tasks.component.html',
   styleUrl: './user-tasks.component.css',
-  imports: [RouterOutlet],
+  imports: [RouterLink, RouterOutlet],
 })
 export class UserTasksComponent {
   private UsersService = inject(UsersService);
-  private TasksService = inject(TasksService);
   userId = input.required<string>();
 
   userName = computed(
     () => this.UsersService.users.find((u) => u.id === this.userId())?.name
   );
-
-  userTasks = computed(() => {
-    const tasks = this.TasksService.allTasks().find(
-      (t) => t.userId === this.userId()
-    );
-    return tasks ? [tasks] : [];
-  });
 }
